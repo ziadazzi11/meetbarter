fabd426b3d425e201f6a148927a4d56268c943c3
Ziad Azzi <ziadazzi11@gmail.com>
2026-02-11 21:48:04 +0200

    fix(backend): import NotificationsModule in SubscriptionsModule

--- backend/src/subscriptions/subscriptions.module.ts
+++ backend/src/subscriptions/subscriptions.module.ts
@@ -4,9 +4,10 @@ import { SubscriptionsService } from './subscriptions.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SubscriptionsScheduler } from './subscriptions.scheduler';
 import { ListingsModule } from '../listings/listings.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-    imports: [PrismaModule, ListingsModule],
+    imports: [PrismaModule, ListingsModule, NotificationsModule],
     controllers: [SubscriptionsController],
     providers: [SubscriptionsService, SubscriptionsScheduler],
     exports: [SubscriptionsService],
